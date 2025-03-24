be5e85819ab81d901e301b86147e1f165e9b73c9
mbg0036 <mbg0036@alu.medac.es>
2025-03-24 12:52:00 +0100

    Crear - Tarjeta_Pregunta

---
+++ src/components/Tarjeta_Pregunta.jsx
@@ -0,0 +1,11 @@
+function Tarjeta_Pregunta() {
+    const [count, setCount] = useState(0)
+
+    return (
+    <h1 className="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    )
+ }
+
+ export default Tarjeta_Pregunta
