576d21d8403f46073fd67f15523bf7b37471434a
JMTerroba <terrobajm@gmail.com>
2025-03-25 14:43:15 +0100

    Controller: aplicado controlador para inicio de sesion y un middleware basico a completar cuando haya mas vistas

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,27 @@
+import prisma from '../config/db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const login = async (req, res) => {
+  const { usuario, password } = req.body;
+
+  try {
+    const user = await prisma.usuario.findUnique({
+      where: { usuario },
+    });
+
+    if (!user) return res.status(404).json({ error: 'Usuario no encontrado' });
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) return res.status(401).json({ error: 'Contraseña incorrecta' });
+
+    const token = jwt.sign({ id: user.id, usuario: user.usuario }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.json({ message: 'Login exitoso', token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error del servidor' });
+  }
+};
