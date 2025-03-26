2faf3e40321de8cf295485f7c684dc6b38aaff2c
mbg0036 <mbg0036@alu.medac.es>
2025-03-26 11:39:37 +0100

    Añadir font y colores

--- src/components/Tarjeta_Pregunta.jsx
+++ src/components/Tarjeta_Pregunta.jsx
@@ -7,10 +7,10 @@ function Tarjeta_Pregunta() {
     // Tablero Madera
     <div className='h-screen w-screen bg-tablero flex items-center justify-center min-h-screen' >
         {/* Fondo Verde */}
-        <div className='bg-[#5B9642] bg-opacity-50 flex items-center justify-center h-screen w-screen' >
+        <div className='bg-verdeClaro bg-opacity-70 flex items-center justify-center h-screen w-screen' >
             {/* Tarjeta */}
-            <section id='tarjeta' className='bg-[#5B9642] flex flex-col h-[80vh] w-[74vw] rounded-lg border-black border-[4px]'>
-                <div id='titulo' className='w-full h-[106px] mt-7 bg-[#213E14] flex items-center'>
+            <section id='tarjeta' className='bg-verdeClaro flex flex-col h-[80vh] w-[74vw] rounded-lg border-black border-[4px]'>
+                <div id='titulo' className='w-full h-[106px] mt-7 bg-verdeOscuro flex items-center'>
                     <h1 className='text-white text-7xl pl-20 font-secular'>GEOGRAFIA</h1>
                 </div>
                 <div id='contenido-tarjeta' className='flex-1 w-full flex justify-between'>
@@ -21,11 +21,11 @@ function Tarjeta_Pregunta() {
                             </h1>
                         </div>
                         </div>    
-                    <div id='respuestas' className='w-1/2 flex flex-col items-center justify-center'>
-                        <h1 className="text-3xl font-bold"> Estados Unidos </h1>
-                        <h1 className="text-3xl font-bold"> Unión Soviética </h1>
-                        <h1 className="text-3xl font-bold"> Alemania </h1>
-                        <h1 className="text-3xl font-bold"> China </h1>
+                    <div id='respuestas' className='w-1/2 flex flex-col items-center justify-center font-lemon'>
+                        <h1 className="bg-white py-2 pt-1 text-black text-3xl font-bold"> Estados Unidos </h1>
+                        <h1 className="bg-white py-2 pt-1 text-black text-3xl font-bold"> Unión Soviética </h1>
+                        <h1 className="bg-white py-2 pt-1 text-black text-3xl font-bold"> Alemania </h1>
+                        <h1 className="bg-white py-2 pt-1 text-black text-3xl font-bold"> China </h1>
                     </div>
                 </div>
             </section>
