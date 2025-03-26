005915f14d154bcec8af07937d513a38e2c9aebb
mbg0036 <mbg0036@alu.medac.es>
2025-03-26 10:46:03 +0100

    Retomar Diseño 26.03

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@
 export default function App() {
   return (
     <div className="h-screen w-screen">
-      <header className="py-6">
+      {/*<header className="py-6">
         <h1 className="text-3xl font-bold text-center text-slate-800">
           Trivial Customizable
         </h1>
       </header>
       <main className="container mx-auto px-4">
   
-      </main>*
-       {/* <Tarjeta_Pregunta></Tarjeta_Pregunta> */}
+      </main>*/}
+        <Tarjeta_Pregunta></Tarjeta_Pregunta> 
     </div> 
     
   
