8a072b48b76057d4d29870f00449db89f036d623
mbg0036 <mbg0036@alu.medac.es>
2025-03-27 11:48:27 +0100

    Probando Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 //Estos son ejemplos de componentes de React cuando desarrolleis otros elimiinad estos
+import HeaderNav from './components/HeaderNav';
 import Tarjeta_Pregunta from './components/Tarjeta_Pregunta';
 import Tarjeta_PreguntaTrasero from './components/Tarjeta_PreguntaTrasero';
 
 export default function App() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen gap-5">
       <header>
-      
+    <HeaderNav></HeaderNav>
       </header>
       <main >
   <Tarjeta_Pregunta></Tarjeta_Pregunta>
---
+++ src/components/HeaderNav.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const HamburgerIcon = () => (
+  <svg
+    className="w-6 h-6"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+);
+
+const HeaderNav = () => {
+  return (
+    <header className="h-32 fixed top-0 left-0 right-0 z-50 w-full bg-[url('/img/pizarra.jpg')] shadow-lg bg-cover bg-center">
+      <div className="container mx-auto h-full px-4 py-3 flex items-center justify-center">
+        {/* Título */}
+        <div className="text-center relative">
+          <h1 className="text-white text-5xl font-pinyon tracking-wide mb-3">
+            Trivial Quiz
+          </h1>
+          <span className="text-black bg-white text-xl font-secular px-10 mt-4 rounded">
+            CUSTOMIZED
+          </span>
+        </div>
+
+        {/* Botón de menú hamburguesa */}
+        <button className="text-white hover:text-gray-300 focus:outline-none absolute right-4">
+          <HamburgerIcon />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderNav;
