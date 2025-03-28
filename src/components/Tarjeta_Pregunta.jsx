9661a9b3140700322ba369fe1cbffc1d8833055d
mbg0036 <mbg0036@alu.medac.es>
2025-03-28 16:02:49 +0100

    Ajustes Maquetación ambas Tarjeta_Pregunta

--- src/components/Tarjeta_Pregunta.jsx
+++ src/components/Tarjeta_Pregunta.jsx
@@ -10,15 +10,15 @@ function Tarjeta_Pregunta() {
         <div className='bg-verdeClaro bg-opacity-70 flex items-center justify-center h-screen w-screen' >
             {/* Tarjeta */}
             <section id='tarjeta' className='bg-verdeClaro flex flex-col h-[80vh] w-[74vw] rounded-lg border-black border-[4px]'>
-                <div id='titulo' className='w-full h-[106px] mt-7 bg-verdeOscuro flex items-center gap-80'>
+                <div id='titulo' className='w-full h-[106px] mt-7 bg-verdeOscuro flex items-center gap-64'>
                     <h1 className='text-white text-7xl pl-20 font-secular [text-shadow:_2px_2px_4px_rgba(0,0,0,0.5)]'>GEOGRAFIA</h1>
-                    <img src="/public/assets/img/queso-color.png" className='w-[160px] pt-10' alt="" />
+                    <img src="/public/assets/img/queso-color-blanco.png" className='w-[160px] pt-10' alt="" />
                 </div>
                 <div id='contenido-tarjeta' className='flex-1 w-full flex justify-between font-lemon'>
                     {/*Pregunta*/}
                     <div id='pregunta' className='w-1/2 ml-14 flex flex-col items-center justify-center'>
                         <div id='marco-pregunta' className='p-7 h-[90%] w-[90%] bg-white bg-opacity-50 rounded-2xl items-center justify-center text-center'>
-                            <h1 className="font-bold text-[50px] text-black text-opacity-90 leading-tight px-4">
+                            <h1 className="font-bold text-[46px] text-black text-opacity-90 leading-tight px-4">
                             ¿Qué país fue el primero en enviar un satélite al espacio?
                             </h1>
                         </div>
--- src/components/Tarjeta_PreguntaTrasero.jsx
+++ src/components/Tarjeta_PreguntaTrasero.jsx
@@ -10,23 +10,23 @@ function Tarjeta_PreguntaTrasero() {
         <div className='bg-verdeClaro bg-opacity-70 flex items-center justify-center h-screen w-screen' >
             {/* Tarjeta */}
             <section id='tarjeta' className='bg-verdeClaro flex flex-col h-[90%] w-[74vw] rounded-lg border-black border-[4px]'>
-                <div id='titulo' className='w-full h-[106px] mt-7 bg-verdeOscuro flex items-center gap-80'> 
+                <div id='titulo' className='w-full h-[106px] mt-7 bg-verdeOscuro flex items-center gap-64'> 
                     <h1 className='text-white text-7xl pl-20 font-secular [text-shadow:_2px_2px_4px_rgba(0,0,0,0.5)]'>GEOGRAFIA</h1>
                     <img src="/public/assets/img/queso-color-blanco.png" className='w-[160px] pt-10' alt="" />
                 </div>
-                <div id='contenido-tarjeta' className='flex-1 flex-col w-full flex items-center font-lemon p-10 gap-6'>
-                    <div id='respuestas' className='w-full flex flex-col items-center justify-center gap-4'>
-                        <button className="w-[80%] px-5 py-4 rounded-xl flex items-center gap-4 text-nowrap bg-white pl-5 text-black text-2xl font-bold opacity-50"> <img className='w-[29.46px] h-[33.45px]' src="/public/assets/img/icono-queso.png" alt="" /> Estados Unidos </button>
-                        <button className="w-[80%] px-5 py-4 rounded-xl flex flex-col gap-3 text-wrap bg-verdeOscuro pl-5 text-white"> 
+                <div id='contenido-tarjeta' className='flex-1 flex-col w-full flex items-center font-lemon p-10 gap-4'>
+                    <div id='respuestas' className='w-full flex flex-col items-center justify-center gap-2'>
+                        <div className="w-[80%] px-5 py-4 rounded-xl flex items-center gap-4 text-nowrap bg-white pl-5 text-black text-2xl font-bold opacity-50"> <img className='w-[29.46px] h-[33.45px]' src="/public/assets/img/icono-queso.png" alt="" /> Estados Unidos </div>
+                        <div className="w-[80%] px-5 py-4 rounded-xl flex flex-col gap-3 text-wrap bg-verdeOscuro pl-5 text-white"> 
                             <div className='flex justify-start gap-4'>
                                 <img className='w-[29.46px] h-[33.45px]' src="/public/assets/img/icono-queso.png" alt="" /><h1 className='text-2xl font-bold'>Unión Soviética</h1>
                             </div>
                             <div>
                                 <p className='text-xl text-left'>El 4 de octubre de 1957, la Unión Soviética fue el primer país en enviar un satélite al espacio. Este satélite se llamaba Sputnik 1 y marcó el inicio de la llamada Carrera Espacial entre la Unión Soviética y Estados Unidos.</p>
                             </div>
-                                </button>
-                        <button className="w-[80%] px-5 py-4 rounded-xl flex items-center gap-4 text-nowrap bg-white pl-5 text-black text-2xl font-bold opacity-50"> <img className='w-[29.46px] h-[33.45px]' src="/public/assets/img/icono-queso.png" alt="" /> Alemania </button>
-                        <button className="w-[80%] px-5 py-4 rounded-xl flex items-center gap-4 text-nowrap bg-red-700 pl-5 text-white text-2xl font-bold"> <img className='w-[29.46px] h-[33.45px]' src="/public/assets/img/icono-queso.png" alt="" /> China </button>
+                                </div>
+                        <div className="w-[80%] px-5 py-4 rounded-xl flex items-center gap-4 text-nowrap bg-white pl-5 text-black text-2xl font-bold opacity-50"> <img className='w-[29.46px] h-[33.45px]' src="/public/assets/img/icono-queso.png" alt="" /> Alemania </div>
+                        <div className="w-[80%] px-5 py-4 rounded-xl flex items-center gap-4 text-nowrap bg-red-700 pl-5 text-white text-2xl font-bold"> <img className='w-[29.46px] h-[33.45px]' src="/public/assets/img/icono-queso.png" alt="" /> China </div>
                     </div>
                     <button className='w-72 rounded-md p-2 bg-white text-black text-2xl hover:bg-black hover:text-white' >SIGUIENTE RONDA</button>
                 </div>
