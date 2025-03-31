5c42fd92c48e3020eb4fab0fcb2a54f96a9fef86
mbg0036 <mbg0036@alu.medac.es>
2025-03-31 12:13:52 +0200

    Creación Tablero
    Componente tablero
    +
    Creación /src/assets/tablero-casillas
    con casillas y vectores svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
+import Tablero from "./components/Tablero";
 
 //Estos son ejemplos de componentes de React cuando desarrolleis otros elimiinad estos
 export default function App() {
   return (
-    <div className="min-h-screen bg-slate-100">
-      <header className="py-6">
-        <h1 className="text-3xl font-bold text-center text-slate-800">
-          Trivial Customizable
-        </h1>
-      </header>
-      <main className="container mx-auto px-4">
-        {/* Your game content will go here */}
-      </main>
+    <div className="w-screen h-screen">
+      <div className="bg-tablero w-full h-full">
+        <Tablero></Tablero>
+      </div>
     </div>
   )
 }
---
+++ src/components/Tablero.jsx
@@ -0,0 +1,14 @@
+
+
+function Tablero() {
+
+
+  return (
+    <h1 className=" w-full h-full flex items-center justify-center ">
+        <img src="/public/assets/img/fondo-tablero.png" className="w-[50%]" alt="" />
+
+    </h1>
+  )
+}
+
+export default Tablero
