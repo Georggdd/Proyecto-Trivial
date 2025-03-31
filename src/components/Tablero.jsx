b420791edbb40da366bf1511574af0ab117cc646
mbg0036 <mbg0036@alu.medac.es>
2025-03-31 14:47:57 +0200

    Añadiendo casillas
    No consigo posicionar las casillas dentro del tablero

--- src/components/Tablero.jsx
+++ src/components/Tablero.jsx
@@ -1,26 +1,30 @@
+import Casilla0 from '/src/components/casillas/Casilla0';
 import Casilla1 from '/src/components/casillas/Casilla1';
 
 function Tablero() {
     return (
-      <div className="relative w-full h-full flex items-center justify-center">
+      <div className="relative w-full h-screen">
         {/* Fondo del tablero */}
         <img
           src="/assets/img/fondo-tablero.png"
           alt="Tablero"
-          className="w-[50%]"
+          className="absolute w-[50%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0"
         />
   
         {/* Imagen path encima del fondo */}
         <img
           src="/assets/img/path.png"
           alt="Path"
-          className="absolute top-1/2 left-1/2 w-[40%] md:w-[40%] 
-                   -translate-x-1/2 -translate-y-1/2 z-10"
+          className="absolute w-[40%] md:w-[40%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10" 
         />
-        <div>
-            <Casilla1 className="absolute top-[60%] left-[50%] w-[40px] h-[40px] z-10"></Casilla1>
-        </div>
+
         
+        
+        <div className="relative w-[50%] h-[80%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
+        <Casilla0 className="absolute w-[10px] left-[10%] top-[40%]"/>
+        <Casilla1 className="absolute w-[10px] left-[65%] top-[30%]"/>
+
+        </div>
       </div>
 
     );
--- src/components/casillas/Casilla0.jsx
+++ src/components/casillas/Casilla0.jsx
@@ -1,6 +1,6 @@
 export default function Casilla0() {
   return (
-    <svg className="w-full h-full" viewBox="0 0 100 100" width="168" height="150" viewBox="0 0 168 150" fill="none" >
+    <svg className="w-full h-full" width="168" height="150" viewBox="0 0 168 150" fill="none" >
 <path d="M42.4459 1L2 75.3204L42.4459 149H126.108L166 75.3204L126.108 1H42.4459Z" fill="white" stroke="black" strokeWidth="2"/>
 </svg>
   );
