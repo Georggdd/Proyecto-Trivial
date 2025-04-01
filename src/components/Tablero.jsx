4261b4d32415f92b67a65f2c8c4cf6052a18d462
mbg0036 <mbg0036@alu.medac.es>
2025-04-01 12:39:38 +0200

    Posicionamiento casillas circular 2

--- src/components/Tablero.jsx
+++ src/components/Tablero.jsx
@@ -31,9 +31,28 @@ import Casilla35 from '/src/components/casillas/Casilla35';
 import Casilla36 from '/src/components/casillas/Casilla36';
 import Casilla37 from '/src/components/casillas/Casilla37';
 import Casilla4 from '/src/components/casillas/Casilla4';
+import Casilla46 from '/src/components/casillas/Casilla46';
+import Casilla47 from '/src/components/casillas/Casilla47';
+import Casilla48 from '/src/components/casillas/Casilla48';
+import Casilla49 from '/src/components/casillas/Casilla49';
 import Casilla5 from '/src/components/casillas/Casilla5';
+import Casilla50 from '/src/components/casillas/Casilla50';
+import Casilla51 from '/src/components/casillas/Casilla51';
+import Casilla52 from '/src/components/casillas/Casilla52';
+import Casilla53 from '/src/components/casillas/Casilla53';
+import Casilla54 from '/src/components/casillas/Casilla54';
+import Casilla55 from '/src/components/casillas/Casilla55';
+import Casilla56 from '/src/components/casillas/Casilla56';
+import Casilla57 from '/src/components/casillas/Casilla57';
+import Casilla58 from '/src/components/casillas/Casilla58';
 import Casilla6 from '/src/components/casillas/Casilla6';
+import Casilla66 from '/src/components/casillas/Casilla66';
+import Casilla67 from '/src/components/casillas/Casilla67';
+import Casilla68 from '/src/components/casillas/Casilla68';
+import Casilla69 from '/src/components/casillas/Casilla69';
 import Casilla7 from '/src/components/casillas/Casilla7';
+import Casilla70 from '/src/components/casillas/Casilla70';
+import Casilla71 from '/src/components/casillas/Casilla71';
 import Casilla72 from '/src/components/casillas/Casilla72';
 import Casilla8 from '/src/components/casillas/Casilla8';
 import Casilla9 from '/src/components/casillas/Casilla9';
@@ -150,9 +169,7 @@ function Tablero() {
           <Casilla30 />
         </div>
         {/* */}
-        <div className="absolute top-[14%] left-1/2 -translate-x-1/2 w-[4%] z-10">
-          <Casilla31 />
-        </div>
+        <div className="absolute top-[14%] left-1/2 -translate-x-1/2 w-[4%] z-10"> <Casilla31 /> </div>
         <div className="absolute top-[14.4%] left-[52.4%] h-[6.5%] z-10"> <Casilla32 /> </div>
         <div className="absolute top-[15.4%] left-[54.2%] h-[6.5%] z-10"> <Casilla33 /> </div>
         <div className="absolute top-[17%] left-[56.3%] h-[6.5%] z-10"> <Casilla34 /> </div>
@@ -160,10 +177,33 @@ function Tablero() {
         <div className="absolute top-[21.5%] left-[59.9%] h-[6.5%] z-10"> <Casilla36 /> </div>
         <div className="absolute top-[24.5%] left-[61.5%] h-[6.5%] z-10"> <Casilla37 /></div>
 
+        {/* */}
+        <div className="absolute bottom-[24.5%] left-[61.5%] h-[6.5%] z-10"> <Casilla46 /> </div>
+        <div className="absolute bottom-[21.5%] left-[59.9%] h-[6.5%] z-10"> <Casilla47 /> </div>
+        <div className="absolute bottom-[19.2%] left-[58.3%] h-[6.5%] z-10"> <Casilla48 /> </div>
+        <div className="absolute bottom-[17%] left-[56.3%] h-[6.5%] z-10"> <Casilla49 /> </div>
+        <div className="absolute bottom-[15.4%] left-[54.2%] h-[6.5%] z-10"> <Casilla50 /> </div>
+        <div className="absolute bottom-[14%] left-[52.4%] h-[6.5%] z-10"> <Casilla51 /></div> 
+        {/* */}
 
-        <div className="absolute top-[14.5%] right-[52.5%] h-[6.5%] z-10">
-          <Casilla72 />
-        </div>
+        <div className="absolute bottom-[14.5%] left-1/2 -translate-x-1/2 w-[4%] z-10"> <Casilla52 /> </div>
+        <div className="absolute bottom-[14.7%] right-[52.4%] h-[6.5%] z-10"> <Casilla53 /> </div>
+        <div className="absolute bottom-[15.8%] right-[54.2%] h-[6.5%] z-10"> <Casilla54 /> </div>
+        <div className="absolute bottom-[17.5%] right-[56.3%] h-[6.5%] z-10"> <Casilla55 /> </div>
+        <div className="absolute bottom-[19.5%] right-[58.3%] h-[6.5%] z-10"> <Casilla56 /> </div>
+        <div className="absolute bottom-[22%] right-[59.9%] h-[6.5%] z-10"> <Casilla57 /> </div>
+        <div className="absolute bottom-[25%] right-[61.5%] h-[6.5%] z-10"> <Casilla58 /></div>
+        {/* */}
+
+        <div className="absolute top-[29%] right-[63.5%] w-[4%] z-10"> <Casilla66 /> </div>
+        <div className="absolute top-[25%] right-[61.5%] h-[6.5%] z-10"> <Casilla67 /> </div>
+        <div className="absolute top-[22%] right-[59.9%] h-[6.5%] z-10"> <Casilla68 /> </div>
+        <div className="absolute top-[19.5%] right-[58.3%] h-[6.5%] z-10"> <Casilla69 /> </div>
+        <div className="absolute top-[17.5%] right-[56.3%] h-[6.5%] z-10"> <Casilla70 /> </div>
+        <div className="absolute top-[15.8%] right-[54.2%] h-[6.5%] z-10"> <Casilla71 /> </div>
+        <div className="absolute top-[14.7%] right-[52.4%] h-[6.5%] z-10"> <Casilla72 /> </div>
+
+
 
 
 
