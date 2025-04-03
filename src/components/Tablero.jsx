d712868231de128ff90c94a164afcae7ca0f7560
mbg0036 <mbg0036@alu.medac.es>
2025-04-03 13:06:31 +0200

    Tablero Responsive 3
    Todas las fichas posicionadas con las proporciones adecuadas para un diseño responsive.

--- src/components/Tablero.jsx
+++ src/components/Tablero.jsx
@@ -28,7 +28,7 @@ function Tablero() {
         <div className="absolute top-[41.5%] right-[27%] w-[8.5%] -translate-x-1/2 -translate-y-1/2 z-10"> <Casilla numero={2} /> </div>
         <div className="absolute top-[38.5%] right-[22%] w-[8.5%] -translate-x-1/2 -translate-y-1/2 z-10"> <Casilla numero={3}/> </div>
         <div className="absolute top-[35.5%] right-[17%] w-[8.5%] -translate-x-1/2 -translate-y-1/2 z-10"> <Casilla numero={4} /> </div>
-        <div className="absolute top-[32.5%] right-[11.8%] w-[8.5%] -translate-x-1/2 -translate-y-1/2 z-10"> <Casilla numero={5} /> </div>
+        <div className="absolute top-[32.5%] right-[12%] w-[8.5%] -translate-x-1/2 -translate-y-1/2 z-10"> <Casilla numero={5} /> </div>
 
         {/* */}
 
@@ -60,6 +60,7 @@ function Tablero() {
         <div className="absolute top-[38.5%] left-[31%] w-[8.5%] -translate-x-1/2 -translate-y-1/2 z-10"> <Casilla numero={23}/> </div>
         <div className="absolute top-[35.5%] left-[26%] w-[8.5%] -translate-x-1/2 -translate-y-1/2 z-10"> <Casilla numero={24} /> </div>
         <div className="absolute top-[32.5%] left-[21%] w-[8.5%] -translate-x-1/2 -translate-y-1/2 z-10"> <Casilla numero={25} /> </div>
+
         {/* */}
         
         <div className="absolute top-[39%] left-1/2 w-[9%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={26} /></div>
@@ -68,64 +69,66 @@ function Tablero() {
         <div className="absolute top-[22%] left-1/2 w-[9%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={29} /></div>
         <div className="absolute top-[16%] left-1/2 w-[9%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={30} /></div>
 
-        {/* 
+        {/* */}
+      
         <div className="absolute top-[9.5%] left-1/2 w-[9%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={31} /></div>
-        <div className="absolute top-[14.4%] left-[52.4%] w-[9%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={32} /></div>
-        <div className="absolute top-[15.4%] left-[54.2%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={33} /></div>
-        <div className="absolute top-[17%] left-[56.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={34} /></div>
-        <div className="absolute top-[19.2%] left-[58.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={35} /></div>
-        <div className="absolute top-[21.5%] left-[59.9%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={36} /></div>
-        <div className="absolute top-[24.5%] left-[61.5%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={37} /></div>
+        <div className="absolute top-[10.5%] right-[36.2%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={32} /></div>
+        <div className="absolute top-[11.8%] right-[30.2%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={33} /></div>
+        <div className="absolute top-[13.8%] right-[25.2%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={34} /></div>
+        <div className="absolute top-[16.5%] right-[20%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={35} /></div>
+        <div className="absolute top-[19.5%] right-[15.5%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={36} /></div>
+        <div className="absolute top-[23%] right-[12%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={37} /></div>
 
-        
+        {/* */}
 
-        <div className="absolute top-[29.5%] left-[63.7%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={38} /></div>
-        <div className="absolute bottom-[58%] left-[65.4%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={39} /></div>
-        <div className="absolute bottom-[54%] left-[66%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={40} /></div>
-        <div className="absolute bottom-[50%] left-[66.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={41} /></div>
-        <div className="absolute bottom-[45.5%] left-[66.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={42} /></div>
-        <div className="absolute bottom-[41%] left-[66%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={43} /></div>
-        <div className="absolute bottom-[36.5%] left-[65.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={44} /></div>
+        <div className="absolute top-[29.5%] right-[5.8%] h-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={38} /></div>
+        <div className="absolute bottom-[56.7%] right-[4%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={39} /></div>
+        <div className="absolute bottom-[52%] right-[2.9%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={40} /></div>
+        <div className="absolute bottom-[47%] right-[2.3%] h-[5.7%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={41} /></div>
+        <div className="absolute bottom-[41.2%] right-[2.5%] h-[6%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={42} /></div>
+        <div className="absolute bottom-[35.5%] right-[2.9%] w-[7.8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={43} /></div>
+        <div className="absolute bottom-[29.6%] right-[4.2%] w-[7.8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={44} /></div>
         
-        -
+        {/* */}
 
-        <div className="absolute bottom-[29.5%] left-[63.5%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={45} /></div>
-        <div className="absolute bottom-[25%] left-[61.5%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={46} /></div>
-        <div className="absolute bottom-[21.8%] left-[59.9%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={47} /></div>
-        <div className="absolute bottom-[19.2%] left-[58.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={48} /></div>
-        <div className="absolute bottom-[17%] left-[56.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={49} /></div>
-        <div className="absolute bottom-[15.4%] left-[54.2%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={50} /></div>
-        <div className="absolute bottom-[14%] left-[52.4%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={51} /></div>
+        <div className="absolute bottom-[19.5%] right-[5.8%] h-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={45} /></div>
+        <div className="absolute bottom-[16.5%] right-[11.9%] w-[7.8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={46} /></div>
+        <div className="absolute bottom-[11.6%] right-[15.2%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={47} /></div>
+        <div className="absolute bottom-[8.8%] right-[20%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={48} /></div>
+        <div className="absolute bottom-[6%] right-[25%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={49} /></div>
+        <div className="absolute bottom-[4.3%] right-[30.7%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={50} /></div>
+        <div className="absolute bottom-[3%] right-[36%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={51} /></div>
         
-       -
+        {/* */}
+       
         <div className="absolute bottom-[3%] left-1/2 w-[9%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={52} /></div>
-        <div className="absolute bottom-[14.7%] right-[52.4%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={53} /></div>
-        <div className="absolute bottom-[15.8%] right-[54.2%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={54} /></div>
-        <div className="absolute bottom-[17.5%] right-[56.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={55} /></div>
-        <div className="absolute bottom-[19.5%] right-[58.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={56} /></div>
-        <div className="absolute bottom-[22%] right-[59.9%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={57} /></div>
-        <div className="absolute bottom-[25%] right-[61.5%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={58} /></div>
+        <div className="absolute bottom-[3%] left-[41.7%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={53} /></div>
+        <div className="absolute bottom-[4.4%] left-[37%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={54} /></div>
+        <div className="absolute bottom-[6.5%] left-[32.4%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={55} /></div>
+        <div className="absolute bottom-[9%] left-[27.5%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={56} /></div>
+        <div className="absolute bottom-[12.4%] left-[23.3%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={57} /></div>
+        <div className="absolute bottom-[16.7%] left-[19.8%] w-[7.8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={58} /></div>
 
-       -
+       {/* */}
 
-        <div className="absolute bottom-[30%] right-[63.7%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={59} /></div>
-        <div className="absolute bottom-[36.5%] right-[65.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={60} /></div>
-        <div className="absolute bottom-[41%] right-[66%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={61} /></div>
-        <div className="absolute bottom-[45.5%] right-[66.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={62} /></div>
-        <div className="absolute bottom-[50%] right-[66.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={63} /></div>
-        <div className="absolute bottom-[54%] right-[66%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={64} /></div>
-        <div className="absolute bottom-[58%] right-[65.4%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={65} /></div>
+        <div className="absolute bottom-[20.5%] left-[15.5%] h-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={59} /></div>
+        <div className="absolute bottom-[30.7%] left-[12.3%] w-[7.8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={60} /></div>
+        <div className="absolute bottom-[36%] left-[11%] w-[7.8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={61} /></div>
+        <div className="absolute bottom-[41.6%] left-[10.3%] w-[7.8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={62} /></div>
+        <div className="absolute bottom-[47%] left-[10.5%] h-[6%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={63} /></div>
+        <div className="absolute bottom-[51.9%] left-[11%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={64} /></div>
+        <div className="absolute bottom-[56.5%] left-[12.5%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={65} /></div>
 
-       -
+       {/* */}
 
-        <div className="absolute top-[29%] right-[63.5%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={66} /></div>
-        <div className="absolute top-[25%] right-[61.5%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={67} /></div>
-        <div className="absolute top-[22%] right-[59.9%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={68} /></div>
-        <div className="absolute top-[19.5%] right-[58.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={69} /></div>
-        <div className="absolute top-[17.5%] right-[56.3%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={70} /></div>
-        <div className="absolute top-[15.8%] right-[54.2%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={71} /></div>
-        <div className="absolute top-[14.7%] right-[52.4%] w-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={72} /></div>        
-        */}
+        <div className="absolute top-[29.3%] left-[15.5%] h-[10%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={66} /></div>
+        <div className="absolute top-[23%] left-[20%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={67} /></div>
+        <div className="absolute top-[19.5%] left-[23.7%] w-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={68} /></div>
+        <div className="absolute top-[16.3%] left-[27.7%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={69} /></div>
+        <div className="absolute top-[14%] left-[32.2%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={70} /></div>
+        <div className="absolute top-[12%] left-[37.2%] h-[8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={71} /></div>
+        <div className="absolute top-[10.7%] left-[41.7%] h-[7.8%] -translate-x-1/2 -translate-y-1/2 z-10"><Casilla numero={72} /></div>
+      
         </div>
       </div>
 
