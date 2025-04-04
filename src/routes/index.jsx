bd6bfd117377f8362bd74cc4dee27b02a07abbf8
LetiCurso <lmf0017@alu.medac.es>
2025-04-04 12:26:35 +0200

    se ve

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from '../pages/Login';
+import VistaRanking from '../pages/VistaRanking';
+
 // Importa aquí las demás páginas a medida que las vayas creando
 
 function AppRoutes() {
   return (
     <Routes>
-
- <Route path="/" element={<Navigate to="/feature_Ranking_Final" /> }/>
-  <Route path="/feature_Ranking_Final" element={<Ranking />} />
-
-
-
-      {/* <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<Navigate to="/VistaRanking" />} />
+      <Route path="/VistaRanking" element={<VistaRanking />} />
+      {/* <Route path="*" element={<h2>Página no encontrada</h2>} /> */}
+     {/* <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} /> */}
 
       {/* Ejemplo:
