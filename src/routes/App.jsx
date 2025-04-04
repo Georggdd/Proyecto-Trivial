a291dd0c75e02aba803a1cc37c9436cdc30c59a8
LetiCurso <lmf0017@alu.medac.es>
2025-04-04 11:37:50 +0200

    organización pages y routes

---
+++ src/routes/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes'; // automáticamente importa routes/index.jsx
+function App() {
+    return (
+      <Router>
+        <AppRoutes />
+      </Router>
+    );
+  }
+  
+  export default App; 
