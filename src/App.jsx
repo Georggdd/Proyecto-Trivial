22c654f6d0fe1e1be112bf4b8bde5a355829ea44
mbg0036 <mbg0036@alu.medac.es>
2025-04-09 14:15:22 +0200

    Añadir Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 
 //Estos son ejemplos de componentes de React cuando desarrolleis otros elimiinad estos
+import Header from './components/Header';
 import Tarjeta_Pregunta from './components/Tarjeta_Pregunta';
 import { preguntas } from './data/preguntas';
 
 export default function App() {
   return (
-    <div className="h-screen w-screen bg-tablero bg-cover bg-no-repeat">
-     {/* <header>
-    <HeaderNav></HeaderNav>
-      </header>   */}
-      <main>
+    <div className="h-screen w-screen">
+      <header>
+        <Header/> 
+      </header>  
+      
+      <main className='flex items-center justify-center bg-tablero bg-cover bg-no-repeat'>
+     
        <Tarjeta_Pregunta pregunta={preguntas[0]}/>
       </main>
       
