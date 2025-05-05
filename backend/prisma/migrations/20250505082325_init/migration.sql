51161e71ac7930a5ad12f30fffe285c122b6e6ae
mbg0036 <mbg0036@alu.medac.es>
2025-05-05 10:34:05 +0200

    Conexión a Workbench

--- backend/prisma/migrations/20250505082325_init/migration.sql
+++ backend/prisma/migrations/20250505082325_init/migration.sql
@@ -1,13 +1,4 @@
 -- CreateTable
-CREATE TABLE `_partidatorespuestapartida` (
-    `A` VARCHAR(191) NOT NULL,
-    `B` VARCHAR(191) NOT NULL,
-
-    INDEX `_PartidaToRespuestaPartida_B_index`(`B`),
-    UNIQUE INDEX `_PartidaToRespuestaPartida_AB_unique`(`A`, `B`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
 CREATE TABLE `casilla` (
     `id` VARCHAR(191) NOT NULL,
     `tipo` ENUM('normal', 'suerte', 'mala_suerte') NOT NULL,
@@ -40,7 +31,19 @@ CREATE TABLE `integrante` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `partida` (
+CREATE TABLE `PartidaRespuestaPartida` (
+    `id` VARCHAR(191) NOT NULL,
+    `partidaId` VARCHAR(191) NOT NULL,
+    `respuestaId` VARCHAR(191) NOT NULL,
+    `correcta` BOOLEAN NOT NULL,
+
+    INDEX `PartidaRespuestaPartida_partidaId_idx`(`partidaId`),
+    INDEX `PartidaRespuestaPartida_respuestaId_idx`(`respuestaId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Partida` (
     `id` VARCHAR(191) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `estado` ENUM('esperando', 'en_curso', 'finalizada') NOT NULL,
@@ -53,12 +56,50 @@ CREATE TABLE `partida` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `pregunta` (
+CREATE TABLE `Categoria` (
     `id` VARCHAR(191) NOT NULL,
-    `asignatura` ENUM('Matematicas', 'Historia', 'Ciencias', 'Lengua', 'Ingles') NOT NULL,
-    `dificultad` ENUM('facil', 'intermedia', 'dificil') NOT NULL,
-    `texto` VARCHAR(191) NOT NULL,
+    `nombre` VARCHAR(191) NOT NULL,
 
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Pregunta` (
+    `id` VARCHAR(191) NOT NULL,
+    `enunciado` VARCHAR(191) NOT NULL,
+    `dificultad` ENUM('facil', 'intermedia', 'dificil') NOT NULL,
+    `puntuacion` INTEGER NOT NULL,
+    `probabilidad` DOUBLE NOT NULL,
+    `explicacion` VARCHAR(191) NULL,
+    `categoriaId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Respuesta` (
+    `id` VARCHAR(191) NOT NULL,
+    `texto` VARCHAR(191) NOT NULL,
+    `esCorrecta` BOOLEAN NOT NULL,
+    `preguntaId` VARCHAR(191) NOT NULL,
+
+    INDEX `Respuesta_preguntaId_fkey`(`preguntaId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `RespuestaPartida` (
+    `id` VARCHAR(191) NOT NULL,
+    `grupoId` VARCHAR(191) NOT NULL,
+    `preguntaId` VARCHAR(191) NOT NULL,
+    `respuestaId` VARCHAR(191) NOT NULL,
+    `esCorrecta` BOOLEAN NOT NULL,
+    `puntosObtenidos` INTEGER NOT NULL,
+    `comodinActivado` BOOLEAN NOT NULL DEFAULT false,
+
+    INDEX `RespuestaPartida_grupoId_fkey`(`grupoId`),
+    INDEX `RespuestaPartida_preguntaId_fkey`(`preguntaId`),
+    INDEX `RespuestaPartida_respuestaId_fkey`(`respuestaId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -71,33 +112,6 @@ CREATE TABLE `puntuaciongrupo` (
 
     UNIQUE INDEX `PuntuacionGrupo_grupoId_key`(`grupoId`),
     INDEX `PuntuacionGrupo_partidaId_fkey`(`partidaId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `respuesta` (
-    `id` VARCHAR(191) NOT NULL,
-    `preguntaId` VARCHAR(191) NOT NULL,
-    `texto` VARCHAR(191) NOT NULL,
-    `esCorrecta` BOOLEAN NOT NULL,
-
-    INDEX `Respuesta_preguntaId_fkey`(`preguntaId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `respuestapartida` (
-    `id` VARCHAR(191) NOT NULL,
-    `grupoId` VARCHAR(191) NOT NULL,
-    `preguntaId` VARCHAR(191) NOT NULL,
-    `respuestaId` VARCHAR(191) NOT NULL,
-    `esCorrecta` BOOLEAN NOT NULL,
-    `puntosObtenidos` INTEGER NOT NULL,
-    `comodinActivado` BOOLEAN NOT NULL DEFAULT false,
-
-    INDEX `RespuestaPartida_grupoId_fkey`(`grupoId`),
-    INDEX `RespuestaPartida_preguntaId_fkey`(`preguntaId`),
-    INDEX `RespuestaPartida_respuestaId_fkey`(`respuestaId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -125,3 +139,15 @@ CREATE TABLE `usuario` (
     UNIQUE INDEX `usuario_usuario_key`(`usuario`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `PartidaRespuestaPartida` ADD CONSTRAINT `PartidaRespuestaPartida_partidaId_fkey` FOREIGN KEY (`partidaId`) REFERENCES `Partida`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PartidaRespuestaPartida` ADD CONSTRAINT `PartidaRespuestaPartida_respuestaId_fkey` FOREIGN KEY (`respuestaId`) REFERENCES `Respuesta`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Pregunta` ADD CONSTRAINT `Pregunta_categoriaId_fkey` FOREIGN KEY (`categoriaId`) REFERENCES `Categoria`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Respuesta` ADD CONSTRAINT `Respuesta_preguntaId_fkey` FOREIGN KEY (`preguntaId`) REFERENCES `Pregunta`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
