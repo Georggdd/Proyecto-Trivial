6c5964c171b70fb5d9dd0eb0f164e6979b4d236d
LetiCurso <lmf0017@alu.medac.es>
2025-05-13 11:28:56 +0200

    service y controller de base datos

--- backend/src/models/ranking/ranking.service.ts
+++ backend/src/models/ranking/ranking.service.ts
@@ -1,17 +1,17 @@
-// ACCEDE A LOS DATOS DEL MODELO GRUPO Y OBTIENE REGISTROS EN FUNCIÓN DE SU ID
+// ACCEDE A LA BASE DE DATOS DEL MODELO GRUPO Y OBTIENE REGISTROS EN FUNCIÓN DE SU ID
 
 import { grupo } from '@prisma/client';
 import { prisma } from '../../../prisma/index';
 
 class GrupoService { //Pascal Case
-//BUSCA UN GRUPO POR SU ID
+//ACCEDE A UN GRUPO POR SU ID
     async findOne(id: string): Promise<grupo | null> {
     return await prisma.grupo.findUnique({
         where: { id }
     });
     }
 
-//DEVUELVE TODOS LOS GRUPOS
+//ACCEDE A TODOS LOS GRUPOS
     async findAll(): Promise<grupo[]>{
         const grupos = await prisma.grupo.findMany()
        
@@ -20,3 +20,4 @@ class GrupoService { //Pascal Case
 }
 
 export const grupoService = new GrupoService(); //CamelCase
+
