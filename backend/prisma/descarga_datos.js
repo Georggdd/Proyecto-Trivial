4fa976c5b01530ac615b2f5199f4c7ac912378f8
LetiCurso <lmf0017@alu.medac.es>
2025-05-22 12:55:10 +0200

    instalación vite, creación .env ajuste de schema.prisma y funciona el acceso a la base de datos para ejecutar descarga_datos.js

--- backend/prisma/descarga_datos.js
+++ backend/prisma/descarga_datos.js
@@ -3,14 +3,15 @@
 // -- documento para intentar sacar los datos de la base de datos creada por JM--
 
 
-const { PrismaClient } = require('@prisma/client'); //para interactura con la BBDD
-const fs = require('fs'); //Para poder leer y escribir
+// descarga_datos.js
+import { PrismaClient } from '@prisma/client';
+import fs from 'fs'; // 
 
 const prisma = new PrismaClient(); //instancia para hacer consultas
 
 async function exportData() {
-  const productos = await prisma.producto.findMany(); //obtiene todos los registros de la tabla
-  fs.writeFileSync('productos.json', JSON.stringify(productos, null, 2)); //lo convierte en Json y los guarda
+  const grupo = await prisma.grupo.findMany(); //obtiene todos los registros de la tabla
+  fs.writeFileSync('productos.json', JSON.stringify(grupo, null, 2)); //lo convierte en Json y los guarda
   console.log('Datos exportados a productos.json');
 }
 
