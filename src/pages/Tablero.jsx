0267f9b82bbffed07c5119793981611573b68e67
JMTerroba <terrobajm@gmail.com>
2025-05-26 05:47:49 +0200

    Terminar Juego + confirmacion

--- src/pages/Tablero.jsx
+++ src/pages/Tablero.jsx
@@ -93,13 +93,6 @@ function Tablero() {
           })}
 
           <Ficha position={fichaPos} />
-
-          <button
-            onClick={() => navigate("/VistaRanking")}
-            className="absolute top-4 right-4 bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800 z-50"
-          >
-            Ver Ranking
-          </button>
         </div>
 
         {/* Mini-ranking */}
